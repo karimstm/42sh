6d819132b8a3e9831527655c40863f3fc9176e17
Abdelkarim Moutik <amoutik@e2r1p4.1337.ma>
2020-01-03 16:19:15 +0100

    alias norming

--- srcs/alias/alias.c
+++ srcs/alias/alias.c
@@ -1,3 +1,15 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   alias.c                                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: amoutik <marvin@42.fr>                     +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2020/01/03 16:16:54 by amoutik           #+#    #+#             */
+/*   Updated: 2020/01/03 16:16:55 by amoutik          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #include "alias.h"
 
 void			init_alias(t_alias_list *list)
@@ -40,15 +52,19 @@ int				is_alias_pattern(char *str)
 
 char			**split_eq(char *str)
 {
-	char	**list;
-	const 	int len = is_alias_pattern(str);
+	char		**list;
+	const int	len = is_alias_pattern(str);
 
 	list = (char **)malloc(sizeof(char *) * 3);
 	list[0] = strndup(str, len);
-	list[1] = strdup(&str[len+1]);
+	list[1] = strdup(&str[len + 1]);
 	list[2] = NULL;
 	return (list);
 }
+
+/*
+** remember to replace these two line with ft_strdel(&tmp) 75 & 76
+*/
 
 void			alias_insert(t_alias_list *list, char *args)
 {
@@ -69,7 +85,7 @@ void			alias_insert(t_alias_list *list, char *args)
 			tmp = current->value;
 			current->value = alias[1];
 			free(tmp);
-			tmp = NULL; // remember to replace these two line with ft_strdel(&tmp)
+			tmp = NULL;
 			return ;
 		}
 		current = current->next;
@@ -78,9 +94,9 @@ void			alias_insert(t_alias_list *list, char *args)
 	free(alias);
 }
 
-void			alias_find(t_alias_list *list, char *alias)
+t_alias			*alias_find(t_alias_list *list, char *alias)
 {
-	t_alias *current;
+	t_alias			*current;
 	const size_t	len = strlen(alias);
 
 	current = NULL;
@@ -91,72 +107,12 @@ void			alias_find(t_alias_list *list, char *alias)
 		if (len == current->alias_len && strcmp(alias, current->alias) == 0)
 		{
 			dprintf(2, "alias: %s='%s'\n", current->alias, current->value);
-			return ;
+			return (current);
 		}
 		current = current->next;
 	}
 	dprintf(2, "42sh: alias: %s: not found\n", alias);
-}
-
-char			*get_alias_value(t_alias_list *list, char *alias, char *start_alias)
-{
-	t_alias			*current;
-	char			*value;
-	char			*tmp;
-	size_t			len;
-
-	current = NULL;
-	value = NULL;
-	if (list && list->head)
-		current = list->head;
-	len = strlen(alias);
-	while (current)
-	{
-		if (len == current->alias_len && strcmp(alias, current->alias) == 0)
-		{
-			value = current->value;
-			//return (value);
-			if (start_alias && strcmp(value, start_alias) == 0)
-				return (start_alias);
-			if ((tmp = get_alias_value(list, value, start_alias)) == NULL)
-				return (value);
-			return (tmp);
-		}
-		current = current->next;
-	}
-	return (value);
-}
-
-void			delete_alias(t_alias_list *list, char *to_find)
-{
-	t_alias			*current;
-	t_alias			*next;
-	t_alias			*prev;
-	const size_t	len = strlen(to_find);
-
-	current = NULL;
-	prev = NULL;
-	if (list && list->head)
-		current = list->head;
-	while (current)
-	{
-		if (len == current->alias_len && strcmp(to_find, current->alias) == 0)
-		{
-			next = current->next;
-			if (current == list->head)
-				list->head = next;
-			if (current == list->tail)
-				list->tail = prev;
-			free(current->alias);
-			free(current->value);
-			free(current);
-			list->node_count--;
-			current = next;
-			continue ;
-		}
-		prev = current;
-		current = current->next;
-	}
+	return (NULL);
 }
 
 void			aliase_prune(t_alias_list *list)
@@ -179,6 +135,72 @@ void			aliase_prune(t_alias_list *list)
 	init_alias(list);
 	free(list);
 	list = NULL;
+}
+
+char			*get_alias_value(t_alias_list *list,
+								char *alias, char *start_alias)
+{
+	t_alias			*current;
+	t_alias			*tmp_alias;
+	char			*value;
+	size_t			len;
+	t_alias_list	*tmp_list;
+
+	current = NULL;
+	value = NULL;
+	current = (list && list->head) ? list->head : NULL;
+	len = strlen(alias);
+	init_alias((tmp_list = (t_alias_list *)malloc(sizeof(t_alias_list))));
+	while (current)
+	{
+		if (len == current->alias_len && strcmp(alias, current->alias) == 0)
+		{
+			if ((tmp_alias = alias_find(tmp_list, alias)))
+			{
+				if (strcmp(start_alias, alias) == 0)
+					value = start_alias;
+				value = tmp_alias->value;
+				break ;
+			}
+			value = current->value;
+			alias_push(tmp_list, strdup(alias), strdup(value));
+			alias = value;
+			len = strlen(alias);
+			current = list->head;
+		}
+		current = current->next;
+	}
+	aliase_prune(tmp_list);
+	return (value);
+}
+
+void			delete_alias(t_alias_list *list, char *to_find)
+{
+	t_alias			*current;
+	t_alias			*next;
+	t_alias			*prev;
+	const size_t	len = strlen(to_find);
+
+	prev = NULL;
+	current = (list && list->head) ? list->head : NULL;
+	while (current)
+	{
+		if (len == current->alias_len && strcmp(to_find, current->alias) == 0)
+		{
+			next = current->next;
+			if (current == list->head)
+				list->head = next;
+			if (current == list->tail)
+				list->tail = prev;
+			free(current->alias);
+			free(current->value);
+			free(current);
+			list->node_count--;
+			current = next;
+		}
+		prev = current;
+		current = current->next;
+	}
 }
 
 void			print_aliases(t_alias_list *list)
@@ -215,10 +237,16 @@ void			ft_alias(char **args, t_alias_list *alias_list)
 	}
 }
 
-int			print_usage()
+int				print_usage(void)
 {
 	dprintf(2, "unalias: usage: unalias [-a] name [name ...]\n");
 	return (2);
+}
+
+int				print_invalid_option(char c)
+{
+	dprintf(2, "42sh: unalias: -%c: invalid option\n", c);
+	return (print_usage());
 }
 
 int				check_option(char **args)
@@ -231,7 +259,7 @@ int				check_option(char **args)
 	i = 0;
 	while (args[i])
 	{
-		str = args[i];
+		str = args[i++];
 		if (str && str[0] == '-')
 		{
 			str++;
@@ -240,14 +268,10 @@ int				check_option(char **args)
 				if (*str == 'a')
 					found = 0;
 				else
-				{
-					dprintf(2, "42sh: unalias: -%c: invalid option\n", *str);
-					return print_usage();
-				}
+					return (print_invalid_option(*str));
 				str++;
 			}
 		}
-		i++;
 	}
 	return (found);
 }
--- srcs/alias/alias.h
+++ srcs/alias/alias.h
@@ -1,4 +1,16 @@
-# ifndef ALIAS_H
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   alias.h                                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: amoutik <marvin@42.fr>                     +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2020/01/03 16:18:40 by amoutik           #+#    #+#             */
+/*   Updated: 2020/01/03 16:18:41 by amoutik          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef ALIAS_H
 # define ALIAS_H
 
 # include <stdlib.h>
@@ -8,11 +20,10 @@
 typedef struct			s_alias
 {
 	char				*alias;
-	char 				*value;
+	char				*value;
 	size_t				alias_len;
 	struct s_alias		*next;
 }						t_alias;
-
 
 typedef struct			s_alias_list
 {
@@ -21,14 +32,13 @@ typedef struct			s_alias_list
 	size_t				node_count;
 }						t_alias_list;
 
-
 /*
 **	alias.c
 */
-
 void					ft_alias(char **args, t_alias_list *list);
 void					init_alias(t_alias_list *list);
 void					ft_unalias(char **args, t_alias_list *list);
-char					*get_alias_value(t_alias_list *list, char *alias, char *start_alias);
+char					*get_alias_value(t_alias_list *list,
+									char *alias, char *start_alias);
 
 #endif
