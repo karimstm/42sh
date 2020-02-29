b1866ab35e7117406aa27e4c0c9398fb671b82f6
Chadi Jamal <cjamal@e2r3p10.1337.ma>
2020-02-29 01:41:27 +0100

    ft_type normed

--- srcs/minishell/ft_type.c
+++ srcs/minishell/ft_type.c
@@ -1,11 +1,23 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_type.c                                          :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: cjamal <cjamal@student.42.fr>              +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2020/02/29 01:37:52 by cjamal            #+#    #+#             */
+/*   Updated: 2020/02/29 01:40:45 by cjamal           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #include "shell.h"
 
-char    *working_path_type(char *cmd)
+char	*working_path_type(char *cmd)
 {
-	char        **all_paths;
-	char        **tmp;
-	char        *full_path;
-    t_variables *var;
+	char			**all_paths;
+	char			**tmp;
+	char			*full_path;
+	t_variables		*var;
 
 	var = get_var("PATH");
 	all_paths = var ? ft_strsplit(var->value, ':') : NULL;
@@ -23,46 +35,46 @@ char    *working_path_type(char *cmd)
 	return (full_path);
 }
 
-char    *is_aliased(char *arg)
+char	*is_aliased(char *arg)
 {
-    t_alias_list    *aliases;
-    t_alias         *cur;
+	t_alias_list	*aliases;
+	t_alias			*cur;
 
-    aliases = get_alias_list(NULL);
-    cur = aliases->head;
-    while (cur)
-    {
-        if (ft_strequ(arg, cur->alias))
-            return (cur->value);
-        cur = cur->next;
-    }
-    return (NULL);
+	aliases = get_alias_list(NULL);
+	cur = aliases->head;
+	while (cur)
+	{
+		if (ft_strequ(arg, cur->alias))
+			return (cur->value);
+		cur = cur->next;
+	}
+	return (NULL);
 }
 
-int    ft_type(char **args)
+int		ft_type(char **args)
 {
-    t_list  *blt;
-    char    *value;
-    int     i;
-    int     ret;
+	t_list			*blt;
+	char			*value;
+	int				i;
+	int				ret;
 
-    blt = get_set_blt(NULL);
-    i = 0;
-    ret = 1;
-    while (args[i])
-    {
-        ret = 0;
-        if (ft_lstsearch(blt, args[i], &check_builtin))
-            ft_printf("%s is a shell builtin\n", args[i]);
-        else if ((value = is_aliased(args[i])))
-            ft_printf("%s is aliased to `%s'\n", args[i], value);
-        else if ((value = ht_search(get_hash_table(0), args[i])))
-            ft_printf("%s is hashed (%s)\n", args[i], value);
-        else if ((value = working_path_type(args[i])))
-            ft_printf("%s is %s\n", args[i], value);
-        else
-            ret = ft_printf("42sh: type: %s: not found\n", args[i]);
-        i++;
-    }
-    return (ret);
+	blt = get_set_blt(NULL);
+	i = 0;
+	ret = 1;
+	while (args[i])
+	{
+		ret = 0;
+		if (ft_lstsearch(blt, args[i], &check_builtin))
+			ft_printf("%s is a shell builtin\n", args[i]);
+		else if ((value = is_aliased(args[i])))
+			ft_printf("%s is aliased to `%s'\n", args[i], value);
+		else if ((value = ht_search(get_hash_table(0), args[i])))
+			ft_printf("%s is hashed (%s)\n", args[i], value);
+		else if ((value = working_path_type(args[i])))
+			ft_printf("%s is %s\n", args[i], value);
+		else
+			ret = ft_printf("42sh: type: %s: not found\n", args[i]);
+		i++;
+	}
+	return (ret);
 }
